43a91e586630dd7f420475441452ed951c689096
MzeAhmed <houdjiva@gmail.com>
2024-03-29 18:18:39 +0100

    :recycle: Update sql files

--- dev/queries.sql
+++ dev/queries.sql
@@ -13,3 +13,32 @@ VALUES (1, 'firstname', 'John'),
        (3, 'lastname', 'Smith'),
        (1, 'birthdate', '1985-01-25 00:00:00'),
        (2, 'birthdate', '1990-01-01 00:00:00');
+
+INSERT INTO roles
+VALUES (1, 'administator'),
+       (2, 'moderator'),
+       (3, 'user');
+
+INSERT INTO capabilities (id, name)
+VALUES (1, 'read'),
+       (2, 'edit_object'),
+       (3, 'create_object'),
+       (4, 'delete_object'),
+       (5, 'edit_user'),
+       (6, 'delete_user'),
+       (7, 'edit_post'),
+       (8, 'delete_post');
+
+INSERT INTO role_capabilities (role_id, capability_id)
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6),
+       (1, 7),
+       (1, 8),
+       (2, 5),
+       (2, 7),
+       (2, 8),
+       (3, 1);
--- dev/schema.sql
+++ dev/schema.sql
@@ -36,11 +36,6 @@ CREATE TABLE roles
 ) DEFAULT CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_520_ci;
 
-INSERT INTO roles
-VALUES (1, 'administator'),
-       (2, 'moderator'),
-       (3, 'user');
-
 CREATE TABLE user_roles
 (
     id      INT AUTO_INCREMENT PRIMARY KEY,
@@ -58,16 +53,6 @@ CREATE TABLE capabilities
 ) DEFAULT CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_520_ci;
 
-INSERT INTO capabilities (id, name)
-VALUES (1, 'read'),
-       (2, 'edit_object'),
-       (3, 'create_object'),
-       (4, 'delete_object'),
-       (5, 'edit_user'),
-       (6, 'delete_user'),
-       (7, 'edit_post'),
-       (8, 'delete_post');
-
 CREATE TABLE role_capabilities
 (
     id            INT PRIMARY KEY AUTO_INCREMENT,
@@ -77,17 +62,3 @@ CREATE TABLE role_capabilities
     FOREIGN KEY (capability_id) REFERENCES capabilities (id)
 ) DEFAULT CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_520_ci;
-
-INSERT INTO role_capabilities (role_id, capability_id)
-VALUES (1, 1),
-       (1, 2),
-       (1, 3),
-       (1, 4),
-       (1, 5),
-       (1, 6),
-       (1, 7),
-       (1, 8),
-       (2, 5),
-       (2, 7),
-       (2, 8),
-       (3, 1);
