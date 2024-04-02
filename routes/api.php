639380cfaafb92e862087b9b7a20321bf7bacea0
MzeAhmed <houdjiva@gmail.com>
2024-04-02 17:09:12 +0200

    :recycle: Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Core\Container;
 use App\Core\Http\Router\Router;
 use App\Http\Middleware\AuthMiddleware;
-use App\Core\Exceptions\RouterException;
 use App\Http\Middleware\NotAuthMiddleware;
 
 $container = null;
@@ -12,7 +11,7 @@ Container::initializeContainer($container);
 
 $router = new Router($container);
 
-$authRoutes = [
+$userRoutes = [
     $router->get(API_BASE_SLUG . '/users', 'App\Http\Controllers\Api\UserController@index'),
     $router->get(API_BASE_SLUG . '/user/:id', 'App\Http\Controllers\Api\UserController@show')->with('id', '[0-9]+'),
     $router->post(API_BASE_SLUG . '/logout', 'App\Http\Controllers\Api\SecurityController@logout'),
@@ -22,7 +21,7 @@ $authRoutes = [
     $router->get(API_BASE_SLUG . '/post/:id', 'App\Http\Controllers\Api\PostController@show')->with('id', '[0-9]+'),
 ];
 
-$notAuthRoutes = [
+$guestRoutes = [
     $router->post(API_BASE_SLUG . '/user/create', 'App\Http\Controllers\Api\UsersController@create'),
     $router->post(API_BASE_SLUG . '/login', 'App\Http\Controllers\Api\SecurityController@login'),
 ];
@@ -31,15 +30,11 @@ $router->get(API_BASE_SLUG . '/', function () {
     echo 'Welcome to Coelacanthe API v1';
 });
 
-$router->addGroupMiddleware($authRoutes, [new AuthMiddleware()]);
-$router->addGroupMiddleware($notAuthRoutes, [new NotAuthMiddleware()]);
+$router->addGroupMiddleware($userRoutes, [new AuthMiddleware()]);
+$router->addGroupMiddleware($guestRoutes, [new NotAuthMiddleware()]);
 
 $router->get('/', function () {
     echo '';
 });
 
-try {
-    $router->run();
-} catch (RouterException $e) {
-    echo $e->getMessage();
-}
+$router->run();
